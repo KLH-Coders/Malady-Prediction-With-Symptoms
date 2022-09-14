6410501e560e0c8737c4cc8902ae635571e02506
N00BM45T3R69 <98582199+N00BM45T3R69@users.noreply.github.com>
2022-09-14 09:08:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+st.title("Malady Prediction Using Symptoms")
+st.header("This is the alpha testing phase stay tuned for further progress 🩺")
+st.write("You'll be able to select symptoms and with a click our backend will run predictions using Data Mining Algorithms")
